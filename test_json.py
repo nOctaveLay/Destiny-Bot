7d6e1f4f310f06dbf9c34cce86d5c22280545dbb
nOctaveLay <wjddkdml1124@naver.com>
2020-10-30 15:30:06 +0900

    Update test_json.py

--- test_json.py
+++ test_json.py
@@ -4,8 +4,35 @@ import os.path
 from datetime import datetime,date,timedelta
 
 
-auth = {'User-Agent':'nOctaveLay'}
+
+def update():
+    auth = {'User-Agent':'nOctaveLay'}
+    res = requests.get("https://api.github.com/repos/nOctaveLay/Destiny-Bot/issues?state=closed",headers = auth)
+    issue_data = res.json()
+    result_data = []
+    today = date.today()
+    today_datetime = datetime.today()
+    update_time_iso = datetime.isoformat(today_datetime,timespec ='seconds')
+    update_time_iso = update_time_iso[update_time_iso.index('T')+1:]
+    result_data.append({"시간":update_time_iso})
+    time_delta = timedelta(days = 8)
+    today_delta = today - time_delta
+
+    # now_time = time.gmtime()
+    # now_time.tm_hour = now_time.tm_hour += 9
+
+    for data in issue_data:
+        end_time = data['closed_at']
+        end_time_iso = end_time[:end_time.index('T')]
+        end_time = date.fromisoformat(end_time_iso)
+        if end_time <= today and end_time > today_delta:
+            dict_date,dict_title = end_time_iso,data['title']
+            string_format = {"날짜": dict_date, "제목": dict_title}
+            result_data.append(string_format)
+    return result_data
+
 today = date.today()
+today_datetime = datetime.today()
 file_name = 'issue_name.txt'
 time_delta = timedelta(days = 8)
 search_range = today - time_delta
@@ -13,44 +40,15 @@ if os.path.isfile(file_name):
     with open(file_name,'r',encoding = 'utf-8') as f:
         issue_data = f.read()
 issue_convert_json = json.loads(issue_data)
+if '시간' in issue_convert_json[0]:
+    text_time = issue_convert_json[0]['시간']
+    text_time = text_time.split(":")
+else: text_time = today
 
-# max_time = search_range
-file_issue_data = list()
-issue_date = date.fromisoformat(issue_element['날짜'])
-if issue_date < today:
-    file_issue_data = update()
-for issue_element in issue_convert_json:
-    issue_date = date.fromisoformat(issue_element['날짜'])
-    if search_range <= issue_date:
-        file_issue_data.append(issue_element)
+issue_date = date.fromisoformat(issue_convert_json[1]['날짜'])
+if issue_date < today or text_time[0] < today_datetime.hour-1:
+    issue_data = update()
 
-issue_list = list()
-for issue_element in issue_data:
-    issue_end_time_iso = issue_element['closed_at']
-    T_index = issue_end_time_iso.find('T')
-    issue_end_time_iso = issue_end_time_iso[:T_index]
-    issue_end_time = date.fromisoformat(issue_end_time_iso)
-    if issue_end_time <= today and search_range < issue_end_time:
-        title = issue_element['title']
-        string_format = {"날짜": issue_end_time_iso, "제목": title}
-        issue_list.append(string_format)
-json_list = json.dumps(issue_list)
+json_list = json.dumps(issue_data)
 with open('issue_name.txt','w',encoding = 'utf-8') as f:
     f.write(json_list)
-
-# today = datetime.today().strftime('%Y-%m-%d')
-# issue_name = 'issue'
-# if today not in issue_data:
-#     issue_data[today] = [issue_name]
-# else:
-#     issue_data[today].append(issue_name)
-# jsonString = json.dumps(issue_data,indent=4)
-
-def update(old_data):
-    res = requests.get("https://api.github.com/repos/nOctaveLay/Destiny-Bot/issues?state=closed",headers = auth)
-    issue_data = res.json()
-    result_data = []
-    for data in issue_data:
-        if data['closed_at']
-        result_data.append(data)
-    return issue_data
