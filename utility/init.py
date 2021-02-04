19950e2b4bf855526ab7d1a6529f7b4cbb40d24e
nOctaveLay <wjddkdml1124@naver.com>
2021-02-04 09:37:32 +0900

    Update init.py
    선각자 추가
    말에 맞게 살짝 말을 변경
    closed #28

--- utility/init.py
+++ utility/init.py
@@ -3,7 +3,7 @@ from datetime import date, datetime
 __all__ = ['change_activity_text_dict','change_raid_text_dict','init_raid','init_activity','init_num']
 
 def change_activity_text_dict(act_num_dict):
-    activity_list = ['공격전','황혼전 시련','시련의 장','갬빗','레이드','예언']
+    activity_list = ['공격전','황혼전 시련','시련의 장','갬빗','레이드','예언','선각자']
     new_text_dict = list()
     today_time = datetime.today().weekday()
     for x in act_num_dict:
@@ -22,6 +22,8 @@ def change_activity_text_dict(act_num_dict):
                 new_text_dict.append(f'5명의 수호자와 함께, 우리는 **레이드**에서 더 거대한 적을 막아야만 해요. {num_text}정도면 충분하다고 선봉대장이 말하네요.')
             elif x == activity_list[5]:
                 new_text_dict.append(f'아홉이 당신을 {num_text}부르고 있어요, **예언**에서요. 빛과 어둠이 무엇일지를 알려준데요.')
+            elif x == activity_list[6]:
+                new_text_dict.append(f'매 한마리가 당신을 보며 {num_text}울부짖고 있어요. **선각자**를 찾고 있나봐요.')
             elif (today_time >4 or today_time < 2) and x == '오시리스의 시련':
                 new_text_dict.append(f'오, 이런. 오시리스가 등대에 불을 밝혀달라고 그러던데, 생각 있어요?, 당장 **오시리스의 시련**으로 {num_text}떠나세요.')
             else:
@@ -49,14 +51,15 @@ def change_raid_text_dict(raid_num_dict):
 
 def init_activity():
     today_time = datetime.today().weekday()
-    activity_list = ['공격전','황혼전 시련','시련의 장','갬빗','레이드','예언']
+    activity_list = ['공격전','황혼전 시련','시련의 장','갬빗','레이드','예언','선각자']
     activity_text_dict ={
         activity_list[0]:'수호자, 파란 빡빡이가 선봉대가 해야 하는 **공격전**을 하라고 부르고 있어요.',
         activity_list[1]:'왜 선봉대에 있는 수호자들이 죽어났는지, 알 거 같네요. **황혼전 시련**을 하라고 자발라가 부르고 있어요.',
         activity_list[2]:'샤크스가 요새 바쁜 일이 있냐고 묻네요, **시련의 장**에서 당신의 실력을 보여달래요.',
         activity_list[3]:'방랑자가 당신을 부르고 있어요. 타락한 수호자들을 막아달라네요, **갬빗**에서요.',
         activity_list[4]:'5명의 수호자와 함께, 우리는 **레이드**에서 더 거대한 적을 막아야만 해요.',
-        activity_list[5]:'아홉이 당신을 부르고 있어요, **예언**에서요. 빛과 어둠이 무엇일지를 알려준데요.'
+        activity_list[5]:'아홉이 당신을 부르고 있어요, **예언**에서요. 빛과 어둠이 무엇일지를 알려준데요.',
+        activity_list[6]:'매 한마리가 당신을 보며 울부짖고 있어요. **선각자**를 찾고 있나봐요.'
     }
     if today_time >4 or today_time < 2:
         activity_list.append('오시리스의 시련')
