987d032b30d7ea9d8704e3b8b43c42b438477cac
nOctaveLay <wjddkdml1124@naver.com>
2021-02-04 13:18:58 +0900

    Update utility.py
    "use.md" 파일 이동.

--- utility/utility.py
+++ utility/utility.py
@@ -15,7 +15,7 @@ def print_dict(choosen_string,choosen_dict):
 #     for _ in range(num):
 #         function()
 
-def usage(file_name = './use.md'):
+def usage(file_name = './data/use.md'):
     string = '사용법에 대해서 설명하겠습니다.\n'
     with open(file_name,'r',encoding = 'utf-8') as f:
         while True:
